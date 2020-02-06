3977bfb3ccaa67a93e5129b2c7b375bc9de63598
taojiatao <1456818115@qq.com>
2020-02-06 10:29:04 +0800

    03

--- 03/src/search.js
+++ 03/src/search.js
@@ -9,7 +9,7 @@ class Search extends React.Component{
     render(){
         return <div className='search-text'>
             Search Text<img src={ logo }/>
-            </div>
+        </div>
     }
 }
 
--- 03/webpack.prod.js
+++ 03/webpack.prod.js
@@ -58,7 +58,14 @@ module.exports = {
                     MiniCssExtractPlugin.loader, // 把css样式生成单文件 与‘style-loader’ 冲突
                     'css-loader',
                     'less-loader',
-                    'postcss-loader'
+                    'postcss-loader',// css前缀优化
+                    {
+                        loader: 'px2rem-loader', // 将px转化成rem
+                        options: {
+                            remUnit:75,
+                            remPrecision:8
+                        }
+                    }
                 ]
             },
             // 使用file-loader
